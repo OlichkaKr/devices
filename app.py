a15ea66e203605027cfa830601095c7cff9c040a
Olga Kr <olichka121kr76@gmail.com>
2019-12-11 21:27:51 +0200

    test2

--- app.py
+++ app.py
@@ -1,17 +1,1 @@
-from dotenv import load_dotenv
-
-from datetime import datetime
-import json
-import os
-import ssl
-import sys
-import time
-
-import paho.mqtt.client as mqtt
-import base64
-import hmac
-import urllib.parse
-import time
-from random import random
-
 print('all works good')
